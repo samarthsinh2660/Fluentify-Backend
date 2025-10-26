a8638db13b4cf686328e6958b59fc1ab93a9043d
samarthsinh2660 <rajuvala80@gmail.com>
2025-10-26 17:10:22 +0530

    add the cors

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const port = process.env.PORT || 5000;
 
 // Configure CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','http://localhost:5174','http://localhost:5173'],
+  origin: ['http://localhost:5173', process.env.CORS_ORIGIN],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
